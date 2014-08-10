a75962d79c8135dac827bc79eaa63e171d7d76c7
Sebastian Wilzbach <sebi@wilzbach.me>
2014-08-10 03:18:28 +0200

    simple detail view

--- client/component-detail/component-detail.js
+++ client/component-detail/component-detail.js
@@ -3,5 +3,19 @@ angular.module('registry')
     return {
         restrict: 'E',
         templateUrl: 'component-detail/component-detail.html',
+        link: function(scope, elem, attrs) {
+
+            console.log(scope);
+            var name = scope.$parent.name;
+
+            components = scope.$parent.components;
+            for(var index in components){
+                // search for package - probably
+                // there is a more efficient way
+                if(components[index].name === name){
+                     scope.c = components[index];
+                }
+            }
+        }
     };
 });
