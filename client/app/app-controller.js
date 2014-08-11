b517a4510502eacae2822be72cc06eef16754377
mhelvens <mhelvens@gmail.com>
2014-08-11 15:38:39 +0200

    Fixed app controller so that detail pages can again be deep-linked with a URL.

--- client/app/app-controller.js
+++ client/app/app-controller.js
@@ -1,24 +1,31 @@
 angular.module('registry').controller('AppController', function ($scope, $route, $routeParams, $location, Component, ComponentFilter) {
 
+
+//	$scope.$watchCollection("components", updateDisplayed); // Why was this here?
+
 	$scope.filter = new ComponentFilter();
 	$scope.components = Component.query();
-
 	$scope.layout = 'table';
-
 	$scope.displayedComponents = [];
-	$scope.$watch("filter", updateDisplayed, true);
-	$scope.$watchCollection("components", updateDisplayed);
 
+	//// How to go to a specific detail page
+	//
 	$scope.switchView = function (name) {
 		$location.path("detail/" + name);
 	};
 
-	function updateDisplayed() {
+	//// Show components that match the current filter
+	//
+	$scope.$watch("filter", function updateDisplayed() {
 		$scope.displayedComponents = $scope.components.filter($scope.filter.match.bind($scope.filter));
-		var path = $location.path();
-		if (path.indexOf("/detail") >= 0) {
+	}, true);
+
+	//// Go back to table view on filter change
+	//
+	$scope.$watch("filter", function updateDisplayed(newFilter, oldFilter) {
+		if ($location.path().indexOf("/detail") >= 0 && !angular.equals(newFilter, oldFilter)) {
 			$location.path("/");
 		}
-	}
+	}, true);
 
 });
