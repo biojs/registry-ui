3afa6aa57efcce5c22d11d3c3830715d1bbea35c
Maximilian Koch <tschaka1904@gmail.com>
2014-09-29 14:38:58 +0100

    Fixed registry card view

--- client/component-cards/component-cards.js
+++ client/component-cards/component-cards.js
@@ -3,5 +3,18 @@ angular.module('registry')
     return {
         restrict: 'E',
         templateUrl: 'component-cards/component-cards.html',
+        replace: true,
+        scope: true,
+        link: function postLink($scope, iElement, iAttrs/*, controller*/) {
+            $scope.filter = $scope.$parent.$eval(iAttrs.filter);
+            $scope.filterFn = $scope.filter.match.bind($scope.filter);
+
+            $scope.hiddenCount = 0;
+            $scope.$watch("filter", function () {
+                $scope.hiddenCount = _.countBy($scope.components, $scope.filter.match, $scope.filter)[false];
+            }, true);
+
+            $scope.notDisplayedInColumn = $scope.filter.notDisplayedInColumn;
+        }
     };
 });
