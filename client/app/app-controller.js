88d97f793b1c8c558281bdb5078505a29d44772e
Sebastian Wilzbach <sebi@wilzbach.me>
2014-10-01 21:07:48 +0200

    search bar adapts to the current view

--- client/app/app-controller.js
+++ client/app/app-controller.js
@@ -1,4 +1,4 @@
-angular.module('registry').controller('AppController', function ($scope, $route, $routeParams, $location, Component, ComponentFilter) {
+angular.module('registry').controller('AppController', function ($scope, $route, $routeParams, $location, Component, ComponentFilter, $rootScope) {
 
 
 //	$scope.$watchCollection("components", updateDisplayed); // Why was this here?
@@ -6,19 +6,34 @@ angular.module('registry').controller('AppController', function ($scope, $route,
 	$scope.filter = new ComponentFilter();
 	$scope.components = Component.query();
 	$scope.layout = 'table';
+	$scope.view = 'list';
 	$scope.displayedComponents = [];
 
 	//// How to go to a specific detail page
 	//
 	$scope.switchView = function (name) {
 		$location.path("detail/" + name);
+    $scope.view = 'detail';
 	};
+
+  $rootScope.$on('$routeChangeSuccess', function(ev,data) {   
+    // TODO: very,very dirty. remove me
+    if(data.loadedTemplateUrl == "component-detail/component-detail.html"){
+      $scope.view = 'detail';
+    }else{
+      $scope.view = 'list';
+    }
+   })
 
 	//// Show components that match the current filter
 	//
 	$scope.$watch("filter", function updateDisplayed() {
 		$scope.displayedComponents = $scope.components.filter($scope.filter.match.bind($scope.filter));
 	}, true);
+
+  $scope.setLayout = function(name){
+    $scope.layout = name;
+  }
 
 	//// Go back to table view on filter change
 	//
