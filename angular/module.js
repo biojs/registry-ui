6d026b1810a555e8c5bd807f0401f4317e032bcc
Sebastian Wilzbach <sebi@wilzbach.me>
2015-01-26 23:14:22 +0100

    support short urls with 'd'

--- angular/module.js
+++ angular/module.js
@@ -1,31 +1,35 @@
 angular.module("registry", ['ngRoute'])
-    .filter("present", function () {
-        return function (item, field) {
-            return item[field] ? "✓" : "✗";
-        }
-    })
-    .filter("componentFilter", function () {
-        return function (array, filter) {
+  .filter("present", function() {
+    return function(item, field) {
+      return item[field] ? "✓" : "✗";
+    }
+  })
+  .filter("componentFilter", function() {
+    return function(array, filter) {
 
-        }
-    })
-    .filter('trusted', ['$sce', function ($sce) {
+    }
+  })
+  .filter('trusted', ['$sce', function($sce) {
     return function(url) {
-        return $sce.trustAsResourceUrl(url);
+      return $sce.trustAsResourceUrl(url);
     };
-    }])
-    .run(function ($rootScope) {
-        $rootScope.constructor.prototype._ = _;
-    })
-    .config(['$routeProvider',
-      function($routeProvider) {
-        $routeProvider.
-          when('/detail/:name', {
-            templateUrl: TEMPLATE_URL + 'component-detail/component-detail.html',
-            controller: 'DetailController'
-          }).
-          otherwise({
-            redirectTo: '/',
-            templateUrl: TEMPLATE_URL + 'component-list/component-list.html',
-          });
-      }])
+  }])
+  .run(function($rootScope) {
+    $rootScope.constructor.prototype._ = _;
+  })
+  .config(['$routeProvider',
+    function($routeProvider) {
+
+      var detailRoute = {
+        templateUrl: TEMPLATE_URL + 'component-detail/component-detail.html',
+        controller: 'DetailController'
+      };
+      $routeProvider.
+      when('/detail/:name',detailRoute)
+      .when('/d/:name',detailRoute)
+      .otherwise({
+        redirectTo: '/',
+        templateUrl: TEMPLATE_URL + 'component-list/component-list.html',
+      });
+    }
+  ])
