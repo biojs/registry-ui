dae05c71b32ab36c46afd11c1524e64eafd30dfb
Sebastian Wilzbach <sebi@wilzbach.me>
2015-01-27 03:41:07 +0100

    edit in plunker (new icons)

--- angular/utils.js
+++ angular/utils.js
@@ -2,9 +2,11 @@ function updateSnippets(c) {
   var baseURL = "http://workmen.biojs.net/demo/" + c.name;
   var jsBinURL = "http://workmen.biojs.net/jsbin/" + c.name;
   var codePenURL = "http://workmen.biojs.net/codepen/" + c.name;
+  var plunkerURL = "http://workmen.biojs.net/plunker/" + c.name;
 
   c.snipURL = baseURL;
   c.selectedSnipURL = baseURL + "/" + c.selectedSnip;
   c.selectedSnipBinURL = jsBinURL + "/" + c.selectedSnip;
   c.selectedSnipCodePenURL = codePenURL + "/" + c.selectedSnip;
+  c.selectedSnipPlunkerURL = plunkerURL + "/" + c.selectedSnip;
 }
