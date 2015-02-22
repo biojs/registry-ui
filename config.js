1aefb6d19ef9ff45835530bb5f3354a67b1a21c7
Sebastian Wilzbach <seb@wilzba.ch>
2015-02-22 02:31:53 +0100

    updated s3 config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // some ugly globals that we use in Angular
 
 // registry: the build script should replace that with workman.biojs.net
-REGISTRY_URL = "http://dev.workmen.biojs.net";
+REGISTRY_URL = "http://dev.workman.biojs.net";
